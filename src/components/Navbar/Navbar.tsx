b44d3604b0c075b6086a247255dff793e055846e
TicklishReboots <67234652+TicklishReboots@users.noreply.github.com>
2024-04-05 21:19:44 +0200

    remove comments from navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,41 +1,5 @@
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuList,
-} from "@/components/ui/navigation-menu";
-import { ThemeToggle } from "../ThemeToggle/ThemeToggle";
 import Link from "next/link";
 import { UserButton } from "@clerk/nextjs";
-
-// const Navbar = () => {
-//   return (
-//     <NavigationMenu className="w-full fixed top-0 border-b-2 border-b-slate-50 bg-background p-2">
-//       <NavigationMenuList className="flex gap-4">
-//         <NavigationMenuItem className="flex items-center">
-//           <Link href={"/"} className="">
-//             {/* <VideoCamIcon /> */}
-//             HOME
-//           </Link>
-//         </NavigationMenuItem>
-//         <NavigationMenuItem className="flex-1  flex items-center mr-auto">
-//           <Link href="/watchlist" className="">
-//             Watchlist
-//           </Link>
-//         </NavigationMenuItem>
-//         <NavigationMenuItem className="flex items-center ml-auto">
-//           <span>Welcome, login to see which movies are showing in Berlin!</span>
-//         </NavigationMenuItem>
-//         <NavigationMenuItem>
-//           <UserButton />
-//         </NavigationMenuItem>
-//         <NavigationMenuItem>
-//           <ThemeToggle />
-//         </NavigationMenuItem>
-//       </NavigationMenuList>
-//     </NavigationMenu>
-//   );
-// };
-// export default Navbar;
 
 const Navbar = () => {
   return (
