35640c9dfa87242fac6f331a68450cc5fb58e134
TicklishReboots <67234652+TicklishReboots@users.noreply.github.com>
2024-04-23 16:55:01 +0200

    add resend emails - emails get sent if user movie is showing in theater - add property for sent_email that can be true or false - improve notifications layout

--- src/app/api/notifications/route.ts
+++ src/app/api/notifications/route.ts
@@ -4,11 +4,14 @@ import dbConnect from "@/lib/mongoose";
 import Movie from "@/models/Movie";
 import User from "@/models/User";
 import { currentUser } from "@clerk/nextjs";
+import { mongo } from "mongoose";
 import { NextRequest, NextResponse } from "next/server";
+import { Resend } from "resend";
 
 export async function GET(req: NextRequest, res: NextResponse) {
   const user = await currentUser();
   const notifications = [];
+  const resend = new Resend(process.env.RESEND_API_KEY);
 
   if (!user || !user.id) {
     return NextResponse.json({ success: false });
@@ -33,6 +36,22 @@ export async function GET(req: NextRequest, res: NextResponse) {
           cinemaLink: foundMovie.cinemaUrl,
         });
       }
+
+      if (foundMovie && !movie.sent_email) {
+        console.log("sending email");
+        resend.emails.send({
+          from: "onboarding@resend.dev",
+          to: "christian.graumann@gmail.com",
+          subject: `${foundMovie.title} is playing in a theater near you!`,
+          html: `<p>Hello ${
+            mongoUser.first_name || "moviemindr user"
+          }</p><p>Your tracked movie ${
+            foundMovie.title
+          } is playing in a theater near you. Log in to find out where can watch it on the big screen again!</p><p>Best,<br />moviemindr team</p>`,
+        });
+        movie.sent_email = true;
+      }
+      await mongoUser.save();
     }
   } catch (error) {
     return NextResponse.json({ success: false });
