4b360e7f05e27c224f9abd73a82b6b14e8151117
TicklishReboots <67234652+TicklishReboots@users.noreply.github.com>
2024-09-21 11:40:15 +0200

    improve movies page - add hooks for tmdbMovies and tmdbSearch - refactor watchlist datafetching - fix tmdb and watchlist routes - fix movie suggestion page

--- src/app/api/tmdb/route.ts
+++ src/app/api/tmdb/route.ts
@@ -2,8 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   const searchParams = req.nextUrl.searchParams;
-  const query = searchParams.get("query");
-  const queryWithoutSpaces = query?.replace(/\s/g, "+");
+  const keyword = searchParams.get("keyword");
+  const queryWithoutSpaces = keyword?.replace(/\s/g, "+");
 
   if (process.env.TMDB_API_KEY_AUTH === undefined) {
     return NextResponse.json({ success: false });
